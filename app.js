db378a7893c74b53c8aa42b3df7ab955e4546a1e
AndreaCatapano <a.catapano96@gmail.com>
2025-03-28 15:06:29 +0100

    feat: added router for posts and linked with app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const postRouter = require("./router/post.js")
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use("/posts", postRouter);
 
 
 app.listen(port, function (){ console.log("Server Attivo")})
