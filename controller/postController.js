0d21824ae708e1889f50a422d52f06220c305542
AndreaCatapano <a.catapano96@gmail.com>
2025-03-28 15:32:17 +0100

    feat: added code for destroy function

--- controller/postController.js
+++ controller/postController.js
@@ -1,36 +1,45 @@
 const posts = require("../data/posts.js");
 
 
-function index (req, res){
+function index(req, res) {
     res.json(posts)
 }
 
-function show (req, res){
+function show(req, res) {
     const id = parseInt(req.params.id);
-    const post = posts.find(post => post.id === id); 
+    const post = posts.find(post => post.id === id);
 
-    if(post){
+    if (post) {
         res.json(post);
     } else {
         res.status(404).json({ message: "Post non trovato" });
     }
 }
 
-function store (req, res){
+function store(req, res) {
     res.send("Funzione Store")
 }
 
-function update (req, res){
+function update(req, res) {
     res.send("Funzione Update")
 }
 
-function patch (req, res){
+function patch(req, res) {
     res.send("Funzione Patch")
 }
 
-function destroy (req, res){
-    res.send("Funzione Delete")
+function destroy(req, res) {
+    const id = parseInt(req.params.id);
+    const postIndex = posts.findIndex(post => post.id === id);
+
+    if (postIndex >= 0) {
+        posts.splice(postIndex, 1);
+        console.log(posts)
+        res.sendStatus(204)
+    } else {
+        res.status(404).json({ message: "Post non trovato" });
+    }
 }
 
 
-module.exports = {index, show, store, update, patch, destroy}
+module.exports = { index, show, store, update, patch, destroy }
