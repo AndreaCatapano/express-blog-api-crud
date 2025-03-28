034a193b08aa82f62f056d503320f50faa8dc865
AndreaCatapano <a.catapano96@gmail.com>
2025-03-28 15:24:01 +0100

    feat: added a response in json for index and show function, in show function added a error response for wrong id

--- controller/postController.js
+++ controller/postController.js
@@ -1,9 +1,19 @@
+const posts = require("../data/posts.js");
+
+
 function index (req, res){
-    res.send("Funzione Index")
+    res.json(posts)
 }
 
 function show (req, res){
-    res.send("Funzione Show")
+    const id = parseInt(req.params.id);
+    const post = posts.find(post => post.id === id); 
+
+    if(post){
+        res.json(post);
+    } else {
+        res.status(404).json({ message: "Post non trovato" });
+    }
 }
 
 function store (req, res){
