016b705cbe5b8c6958d76a1f1a81018db5290c11
AndreaCatapano <a.catapano96@gmail.com>
2025-04-01 14:22:12 +0200

    feat: added notfound fuction for error 404

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const postRouter = require("./router/post.js")
+const errorHandler = require("./middleware/errors-handler.js");
+const notfound = require("./middleware/notfound.js");
+
 const app = express();
 const port = 3001;
 
@@ -7,5 +10,9 @@ app.use(express.json());
 app.use(express.static('../public'))
 app.use("/posts", postRouter);
 
+
+
+app.use(notfound);
+app.use(errorHandler);
 
 app.listen(port, function (){ console.log("Server Attivo")})
